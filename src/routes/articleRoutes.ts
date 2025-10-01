7f7cd77bd0012ac37c0dc5d51c88d086d6e61e6c
gemayc <gemita284@hotmail.com>
2025-10-01 13:26:47 +0200

    feat(articles): add validator for POST and PUT requests

--- src/routes/articleRoutes.ts
+++ src/routes/articleRoutes.ts
@@ -1,22 +1,20 @@
 import express from 'express';
-import {
-  getAllArticles,
-  getArticleById,
-  deleteArticle,
-  createArticle,
-  updateArticle
-} from '../controllers/ArticleController.js';
+import {getAllArticles,getArticleById, deleteArticle,createArticle,updateArticle} from '../controllers/ArticleController.js';
 import { authMiddleware, requireRole } from '../middlewares/auth.js';
+import { createArticleValidators, updateArticleValidators, idParamValidators } from '../validators/articleValidators.js';
+import { checkValidations } from "../middlewares/articleMiddlewares.js";
+
+
 
 const articleRouter = express.Router();
 
 // Rutas públicas (sin autenticación)
 articleRouter.get("/", getAllArticles);
-articleRouter.get("/:id", getArticleById);
+articleRouter.get("/:id", getArticleById, idParamValidators,checkValidations);
 
 // Rutas protegidas (requieren autenticación)
-articleRouter.post("/", authMiddleware, createArticle);
-articleRouter.put("/:id", authMiddleware, updateArticle);
+articleRouter.post("/", authMiddleware, createArticle, createArticleValidators, checkValidations);
+articleRouter.put("/:id", authMiddleware, updateArticle, updateArticleValidators, checkValidations);
 articleRouter.delete("/:id", authMiddleware, deleteArticle);
 
 // articleRouter.delete("/:id", authMiddleware, requireRole("admin"), deleteArticle);
