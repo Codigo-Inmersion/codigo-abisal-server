8bbe666cf519190a1f07d6fdbb876865a9a4b079
gemayc <gemita284@hotmail.com>
2025-10-02 23:24:16 +0200

    fix(articles): validate requests before controllers to prevent 500 errors

--- src/routes/articleRoutes.ts
+++ src/routes/articleRoutes.ts
@@ -10,12 +10,12 @@ const articleRouter = express.Router();
 
 // Rutas públicas (sin autenticación)
 articleRouter.get("/", getAllArticles);
-articleRouter.get("/:id", getArticleById, idParamValidators,checkValidations);
+articleRouter.get("/:id", idParamValidators, checkValidations, getArticleById,);
 
 // Rutas protegidas (handleValidation, createArticle, createArticleValidators, checkValidations);
-articleRouter.post("/", authMiddleware, requireRole("admin"),  createArticle, handleValidation, createArticleValidators,);
-articleRouter.put("/:id", authMiddleware, requireRole("admin"),  handleValidation, updateArticle, updateArticleValidators, checkValidations);
-articleRouter.delete("/:id",authMiddleware, requireRole("admin"),  handleValidation, deleteArticle);
+articleRouter.post("/", authMiddleware, requireRole("admin"), createArticleValidators, handleValidation, createArticle,);
+articleRouter.put("/:id", authMiddleware, requireRole("admin"), idParamValidators, updateArticleValidators, handleValidation, checkValidations, updateArticle,);
+articleRouter.delete("/:id",authMiddleware, requireRole("admin"), idParamValidators,  handleValidation, deleteArticle);
 
 
 export default articleRouter;
